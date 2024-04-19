47ad277b683fbd21246f2792f6bdf9fb7eafd37e
Karolina <139314432+KarolinaTaran@users.noreply.github.com>
2024-04-19 16:30:29 +0200

    Initial commit

---
+++ src/components/errorMessage/ErrorMessage.jsx
@@ -0,0 +1,12 @@
+const ErrorMessage = () => {
+  return (
+    <div>
+      <p>
+        Oops! Something went wrong while fetching images. Please refresh the
+        page and try again.
+      </p>
+    </div>
+  );
+};
+
+export default ErrorMessage;
